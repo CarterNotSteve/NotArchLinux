9f4ea3e142f7379a65877d89f8c0e319b9b34764
CarterNotSteve <carterjw2005@gmail.com>
2021-09-30 18:24:01 +0000

    elifs and exit method

---
+++ README.md
@@ -0,0 +1,10 @@
+# NotArchLinux
+
+NotArchLinux (henceforth referred to as NAL) is a recreation of a linux distribution, reminicent of bash (almost a direct copy).
+
+### Legalese
+For details of the legal stuff, see `license.txt`.
+
+### Documentation/Usage
+As of version v0.0.5 the follow commands and their usage is as follows:
+ - `mv` - Usage: `mv [file-from] [file/folder-to]`
--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import time
 import ctools
 import os
 import shutil
-""""
+"""
 ctools.util.print("Booting kernel processes...\n")
 time.sleep(4)
 ctools.util.print("starting core processes...\n")
 time.sleep(0.5)
 
 ctools.util.print("Starting init calls...\n")
-"""
-reg=open('unypw.reg','r+')
-usr=reg.readlines()
-guest = True
-PATH="root"
-"""
+
+
 ctools.util.print("kernel processes: ")
 time.sleep(0.7)
 ctools.util.print("[\033[94mOK!\033[0m]\n")
 time.sleep(2)
-"""
+
 ctools.util.print("Welcome to \033[94mNotArch\033[0m!\n")
 time.sleep(2)
 os.system('clear')
+"""
+
+reg=open('unypw.reg','r+')
+usr=reg.readlines()
+guest = True
+PATH="root"
 
 while guest:
     un=input("Username: ")
@@ -49,10 +51,10 @@ while not guest:
         print("  ".join(os.listdir("filesystem/"+PATH)))
         #ctools.util.print(" ".join(["".join(f[2]) for f in os.walk('filesystem/'+PATH)] )+" \n")
 
-    if cmd == "pwd":
+    elif cmd == "pwd":
         print(PATH)
 
-    if allcmd[0] == "cat":
+    elif allcmd[0] == "cat":
         with open("filesystem/"+PATH+allcmd[1], "r") as o:
             if allcmd[1] in os.listdir("filesystem/"+PATH):
                 k=o.readlines()
@@ -61,7 +63,7 @@ while not guest:
                     ctools.util.print(i)
                 print()
 
-    if allcmd[0] == "cd" and len(allcmd) > 1:
+    elif allcmd[0] == "cd" and len(allcmd) > 1:
         if allcmd[1] in os.listdir("filesystem/"+PATH):
             PATH = PATH + "/" + allcmd[1] + "/"
 
@@ -82,14 +84,14 @@ while not guest:
         else:
             ctools.util.print("No file or directory with that title exists!\n")
 
-    if allcmd[0] == "mkdir":
+    elif allcmd[0] == "mkdir":
         os.mkdir("filesystem/"+PATH+"/"+allcmd[1])
 
-    if allcmd[0] == "touch":
+    elif allcmd[0] == "touch":
         with open("filesystem/"+PATH+"/"+allcmd[1], "w+") as i:
             continue
 
-    if allcmd[0] == "mv":
+    elif allcmd[0] == "mv":
         try:
             if allcmd[2] != "..":
                 shutil.move("filesystem/"+PATH+allcmd[1], "filesystem/"+PATH+allcmd[2])
@@ -98,5 +100,8 @@ while not guest:
         except:
             ctools.util.print("dev messed up here!\n")
 
-    if cmd == "clear":
+    elif cmd == "exit":
+        break
+
+    elif cmd == "clear":
         os.system('clear')
